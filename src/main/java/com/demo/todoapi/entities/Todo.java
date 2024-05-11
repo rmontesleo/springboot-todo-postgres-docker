9283eb72584c676fddf2b5d18a64b79a3e4bcf25
Leopoldo Ramirez <rmontesleo@gmail.com>
2024-05-10 18:50:37 -0600

    backup project. pending solve issue

--- src/main/java/com/demo/todoapi/entities/Todo.java
+++ src/main/java/com/demo/todoapi/entities/Todo.java
@@ -1,12 +1,25 @@
 package com.demo.todoapi.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
 import org.hibernate.Hibernate;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table( name ="todos")
